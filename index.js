c1d744be98b8a7136107834a5a857e8a1c656773
Thomas Delattre <delattre.thomas@cegetel.net>
2022-01-05 16:40:27 +0100

    colorGenerator

---
+++ index.js
@@ -0,0 +1,14 @@
+function colorGenerator() {
+
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    const color = `rgb(${r}, ${g}, ${b})`;
+
+    document.body.style.background = color;
+    document.body.innerHTML = `<h2>${color}</h2>`
+}
+
+
+setInterval(colorGenerator, 2000);
